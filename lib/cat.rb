15026562a47b785eb08e37c3531055cf5a5f11c0
Michael Nnaji <mnnaji3@uwo.ca>
2020-04-30 00:38:07 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,20 @@
 class Cat
+ attr_accessor  :owner, :mood
+ attr_reader  :name
+
+@@all = []
+
+def initialize(name,owner)
+  @name = name
+  @owner = owner
+  @mood = 'nervous'
+  @@all << self
+
+end
+
+def self.all
+  @@all
   # code goes here
+end
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,80 @@
 class Owner
+  attr_reader :name, :species
+
+@@all = []
+
+def initialize(name)
+  @species = 'human'
+  @name = name
+  @@all << self
   # code goes here
+end
+
+def say_species
+  "I am a #{self.species}."
+end
+
+def self.all
+  @@all
+end
+
+def self.count
+  @@all.size
+end
+
+def self.reset_all
+  @@all = []
+end
+
+def cats
+  Cat.all.select do |cat|
+    cat.owner == self
+  end
+end
+
+def dogs
+  Dog.all.select do |dog|
+    dog.owner == self
+  end
+end
+
+def buy_cat(name)
+  new_cat = Cat.new(name,self)
+
+end
+
+def buy_dog(name)
+  new_dog = Dog.new(name,self)
+end
+
+def walk_dogs
+  dogs.each do |dog|
+    dog.mood = "happy"
+  end
+end
+
+def feed_cats
+  cats.each do |cat|
+    cat.mood = "happy"
+  end
+end
+
+def sell_pets
+  cats.each do |cat|
+    cat.mood = "nervous"
+    cat.owner = nil
+  end
+  dogs.each do |dog|
+    dog.mood = "nervous"
+    dog.owner = nil
+  end
+end
+
+def list_pets
+
+ return "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+end
+
+
+
 end
